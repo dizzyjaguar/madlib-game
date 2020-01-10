1e6a73c454e89aedcfed6e32c99d0159a2749d5c
Eli Zevin <dizzyjaguar@gmail.com>
2020-01-09 20:56:31 -0800

    halfway done writing the javascript function, needs to finish still

---
+++ app.js
@@ -0,0 +1,49 @@
+function revealMadLib() {
+    const inputAdjective1 = document.getElementById('input-adjective1');
+    const outputAdjective1 = inputAdjective1.Value;
+    adjective1.textContent = outputAdjective1;
+    
+    const inputFantasySpecies1 = document.getElementById('input-fantasy-species1');
+    const outputFantasySpecies1 = inputFantasySpecies1.Value;
+    fantasySpecies1.textContent = outputFantasySpecies1;
+
+    const inputAdjective2 = document.getElementById('input-adjective2');
+    const outputAdjective2 = inputAdjective2.Value;
+    adjective2.textContent = outputAdjective2;
+
+    //BELOW is where i left off, I need to add the rest of the variables 
+    //linking to the input IDs in HTML just like Above
+
+    const inputAdjective1 = document.getElementById('input-adjective1');
+    const outputAdjective1 = inputAdjective1.Value;
+    adjective1.textContent = outputAdjective1;
+
+    const inputAdjective1 = document.getElementById('input-adjective1');
+    const outputAdjective1 = inputAdjective1.Value;
+    adjective1.textContent = outputAdjective1;
+
+    const inputAdjective1 = document.getElementById('input-adjective1');
+    const outputAdjective1 = inputAdjective1.Value;
+    adjective1.textContent = outputAdjective1;
+
+    const inputAdjective1 = document.getElementById('input-adjective1');
+    const outputAdjective1 = inputAdjective1.Value;
+    adjective1.textContent = outputAdjective1;
+
+    const inputAdjective1 = document.getElementById('input-adjective1');
+    const outputAdjective1 = inputAdjective1.Value;
+    adjective1.textContent = outputAdjective1;
+
+    const inputAdjective1 = document.getElementById('input-adjective1');
+    const outputAdjective1 = inputAdjective1.Value;
+    adjective1.textContent = outputAdjective1;
+
+    const inputAdjective1 = document.getElementById('input-adjective1');
+    const outputAdjective1 = inputAdjective1.Value;
+    adjective1.textContent = outputAdjective1;
+}
+
+
+
+
+button.addEventListener('click')
