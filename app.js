27fcd77841b2d580c1274e049b5c7a199801c242
Eli Zevin <dizzyjaguar@gmail.com>
2020-01-10 14:30:12 -0800

    add css to everything, now it looks beautiful, but some of the variables are NOT working

--- app.js
+++ app.js
@@ -1,49 +1,50 @@
 function revealMadLib() {
+    //word 1
     const inputAdjective1 = document.getElementById('input-adjective1');
-    const outputAdjective1 = inputAdjective1.Value;
+    const outputAdjective1 = inputAdjective1.value;
     adjective1.textContent = outputAdjective1;
-    
+    //word 2 
     const inputFantasySpecies1 = document.getElementById('input-fantasy-species1');
-    const outputFantasySpecies1 = inputFantasySpecies1.Value;
+    const outputFantasySpecies1 = inputFantasySpecies1.value;
     fantasySpecies1.textContent = outputFantasySpecies1;
-
+    //word 3
     const inputAdjective2 = document.getElementById('input-adjective2');
-    const outputAdjective2 = inputAdjective2.Value;
+    const outputAdjective2 = inputAdjective2.value;
     adjective2.textContent = outputAdjective2;
+    //word 4
+    const inputPastTenseVerb1 = document.getElementById('input-past-tense-verb1');
+    const outputPastTenseVerb1 = inputPastTenseVerb1.value;
+    pastTenseVerb1.textContent = outputPastTenseVerb1;
+    //word 5
+    const inputNaturalMaterial1 = document.getElementById('input-natural-material1');
+    const outputNaturalMaterial1 = inputNaturalMaterial1.value;
+    naturalMaterial1.textContent = outputNaturalMaterial1;
+    //word 6
+    const inputNaturalMaterial2 = document.getElementById('input-natural-material2');
+    const outputNaturalMaterial2 = inputNaturalMaterial2.value;
+    naturalMaterial2.textContent = outputNaturalMaterial2;
+    //word 7
+    const inputMarijuanaStrain1 = document.getElementById('input-marijuana-strain-1');
+    const outputMarijuanaStrain1 = inputMarijuanaStrain1.value;
+    marijuanaStrain1.textContent = outputMarijuanaStrain1;
+    //word 8
+    const inputAdjective3 = document.getElementById('input-adjective3');
+    const outputAdjective3 = inputAdjective3.value;
+    adjective3.textContent = outputAdjective3;
+    //word 9
+    const inputFamousPerson1 = document.getElementById('input-name-of-famous-person');
+    const outputFamousPerson1 = inputFamousPerson1.value;
+    famousPerson.textContent = outputFamousPerson1;
+    //word 10
+    const inputFantasyCountry = document.getElementById('input-fantasy-country');
+    const outputFantasyCountry = inputFantasyCountry.value;
+    fantasyCountry.textContent = outputFantasyCountry;
 
-    //BELOW is where i left off, I need to add the rest of the variables 
-    //linking to the input IDs in HTML just like Above
-
-    const inputAdjective1 = document.getElementById('input-adjective1');
-    const outputAdjective1 = inputAdjective1.Value;
-    adjective1.textContent = outputAdjective1;
-
-    const inputAdjective1 = document.getElementById('input-adjective1');
-    const outputAdjective1 = inputAdjective1.Value;
-    adjective1.textContent = outputAdjective1;
-
-    const inputAdjective1 = document.getElementById('input-adjective1');
-    const outputAdjective1 = inputAdjective1.Value;
-    adjective1.textContent = outputAdjective1;
-
-    const inputAdjective1 = document.getElementById('input-adjective1');
-    const outputAdjective1 = inputAdjective1.Value;
-    adjective1.textContent = outputAdjective1;
-
-    const inputAdjective1 = document.getElementById('input-adjective1');
-    const outputAdjective1 = inputAdjective1.Value;
-    adjective1.textContent = outputAdjective1;
-
-    const inputAdjective1 = document.getElementById('input-adjective1');
-    const outputAdjective1 = inputAdjective1.Value;
-    adjective1.textContent = outputAdjective1;
-
-    const inputAdjective1 = document.getElementById('input-adjective1');
-    const outputAdjective1 = inputAdjective1.Value;
-    adjective1.textContent = outputAdjective1;
+    
 }
 
 
 
-
-button.addEventListener('click')
+//get the Element Id of the button and add an Event Listener for when
+// the user clicks the fucntion revealMadlib triggers
+document.getElementById("display-madlib").addEventListener("click", revealMadLib);
